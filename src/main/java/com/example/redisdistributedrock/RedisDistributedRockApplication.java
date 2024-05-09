c093c555762b338e30cae504f7d708d76e15ad5b
신희성 <vvwv3092@gmail.com>
2024-05-09 16:06:18 +0900

    init

---
+++ src/main/java/com/example/redisdistributedrock/RedisDistributedRockApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisdistributedrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDistributedRockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDistributedRockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redisdistributedrock/RedisDistributedRockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisdistributedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDistributedRockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
